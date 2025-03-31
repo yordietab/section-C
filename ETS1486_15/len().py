faf531c1b206872b6d2aac4b5bb0d81c20c3d126
Yordi <167332572+yordietab@users.noreply.github.com>
2025-03-31 08:51:44 +0300

    Create len().py

---
+++ ETS1486_15/len().py
@@ -0,0 +1,11 @@
+#Example 1: finding the length of a string 
+text = "Python"
+print(len(text))  # Output: 6
+
+#Example 2: finding the length of a list
+numbers = [10, 20, 30, 40]
+print(len(numbers))  # Output: 4
+
+#Example 3: finding the number of a keys in a dictionary 
+student = {"name": "Alice", "age": 25, "grade": "A"}
+print(len(student))  # Output: 3
