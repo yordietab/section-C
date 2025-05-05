180f7c080803449d26305ae39b93538936da1713
Yordi <167332572+yordietab@users.noreply.github.com>
2025-05-05 22:44:59 +0300

    Create Dictionary.py

---
+++ ETS1486_15/Dictionary.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "name": "Yordanos",
+    "age": 21,
+    "major": "Engineering"
+}
+print(my_dict["name"])  # Output: Yordanos
+
+#adding or updating a new key pair
+my_dict["grade"] = "A"  # Adds a new key-value pair
+
